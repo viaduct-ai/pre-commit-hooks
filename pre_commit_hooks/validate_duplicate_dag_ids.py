3dc8f1cc40b130cd292c04e55e5a30f05baff0c9
Jay Bhambhani <jay@viaduct.ai>
2021-03-04 15:51:49 -0500

    validation for duplicate dag ids

---
+++ pre_commit_hooks/validate_duplicate_dag_ids.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+
+import argparse
+import io
+import sys
+
+import json
+from airflow.models import DagBag
+from collections import Counter
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-m',
+        '--multi',
+        '--allow-multiple-documents',
+        action='store_true',
+    )
+
+    args = parser.parse_args(argv)
+
+    dupes = find_duplicate_dags()
+    if dupes:
+        print(f'Duplicate dag ids: {dupes} found!')
+        return 1
+    return 0
+
+def find_duplicate_dags():
+    dagbag = DagBag()
+    dag_stat = [stat.dags.strip('][').strip('"').split(', ')  for stat in dagbag.dagbag_stats]
+    counts = Counter([dag_id for stat in dag_stats for dag_id in stat])
+    return [dag_id for dag_id, count in counts if count > 1]
+
+if __name__ == '__main__':
+    sys.exit(main())
