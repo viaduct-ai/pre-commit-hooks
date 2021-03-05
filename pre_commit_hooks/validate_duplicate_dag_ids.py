b28a18aa8fb29b1abcb95c2ab51c00f9f52b18fc
Jay Bhambhani <jay@viaduct.ai>
2021-03-04 22:54:31 -0500

    per johns comments - run make on pre-commit

--- pre_commit_hooks/validate_duplicate_dag_ids.py
+++ pre_commit_hooks/validate_duplicate_dag_ids.py
@@ -11,18 +11,12 @@ from collections import Counter
 
 def main(argv=None):
 
-    dupes = find_duplicate_dags()
-    if dupes:
+    try:
+        subprocess.Popen(["make pre-commit"], stdout=subprocess.PIPE)
+    except AssertionError:
         print(f'Duplicate dag ids: {dupes} found!')
         return 1
     return 0
-
-def find_duplicate_dags():
-    dagbag = DagBag()
-    # dagbag stats are the only dagbag collection that haven't deduped
-    dag_stats = [stat.dags.strip('][').strip('"').split(', ')  for stat in dagbag.dagbag_stats]
-    counts = Counter([dag_id for stat in dag_stats for dag_id in stat])
-    return [dag_id for dag_id, count in counts if count > 1]
 
 if __name__ == '__main__':
     sys.exit(main())
